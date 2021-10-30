c51157188075cfc57d012c628172acb26e5c5451
akaash67 <md.akash6724@gmail.com>
2021-10-30 13:53:01 +0600

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const ObjectId = require('mongodb').ObjectId;
 const { MongoClient } = require("mongodb");
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -47,13 +48,22 @@ async function run() {
       res.send(car)
     })
 
-    app.get('/services/:service_name', async (req, res) =>{
+    // app.get('/services/:service_name', async (req, res) =>{
+    //   const id = req.body
+    //   console.log(id)
+    // })
+
+    app.get('/services/place-order/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      // console.log('load user with id: ', id);
+      console.log(service)
+      res.send(service);
+  })
+
+    app.put('/services/place-order/:services_id', async (req, res)=>{
       const id = req.body
-      console.log(id)
-    })
-
-    app.put('/services/place-order/:services_name', async (req, res)=>{
-
     })
 
   } finally {
